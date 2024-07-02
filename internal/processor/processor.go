f30098f8b40ce81f46268497454de23951a7fa00
Daniil Cherednik <dcherednik@ydb.tech>
2024-07-02 15:13:04 +0200

    Move table writer code in to processor

--- internal/processor/processor.go
+++ internal/processor/processor.go
@@ -1,6 +1,7 @@
 package processor
 
 import (
+	"aardappel/internal/dst_table"
 	"aardappel/internal/hb_tracker"
 	"aardappel/internal/tx_queue"
 	"aardappel/internal/types"
@@ -9,6 +10,7 @@ import (
 	"fmt"
 	"github.com/ydb-platform/ydb-go-sdk/v3/table"
 	"github.com/ydb-platform/ydb-go-sdk/v3/table/result/named"
+	ydbTypes "github.com/ydb-platform/ydb-go-sdk/v3/table/types"
 	"go.uber.org/zap"
 	"time"
 )
@@ -19,6 +21,7 @@ type Processor struct {
 	txQueue         *tx_queue.TxQueue
 	dstServerClient table.Client
 	lastStep        uint64
+	stateStoreQuery string
 }
 
 type Channel interface {
@@ -31,12 +34,24 @@ type TxBatch struct {
 	Hb     types.HbData
 }
 
+func createStateStoreQuery(stateTable string) string {
+	return fmt.Sprintf(`
+UPSERT INTO
+    %v
+    (id, step_id, tx_id)
+VALUES
+    (0, $stateStepId, $stateTxId);
+`, stateTable)
+}
+
 func NewProcessor(ctx context.Context, total int, stateTable string, client table.Client) (*Processor, error) {
 	var p Processor
 	p.hbTracker = hb_tracker.NewHeartBeatTracker(total)
 	p.txChannel = make(chan func() error, 100)
 	p.txQueue = tx_queue.NewTxQueue()
 	p.dstServerClient = client
+	p.stateStoreQuery = createStateStoreQuery(stateTable)
+
 	stateQuery := fmt.Sprintf("SELECT step_id FROM %v WHERE id = 0;", stateTable)
 	var step *uint64
 	err := p.dstServerClient.DoTx(ctx,
@@ -124,4 +139,61 @@ func (processor *Processor) FormatTx(ctx context.Context) (*TxBatch, error) {
 			zap.Uint32("tableId:", data.TableId))
 	}
 	return &TxBatch{TxData: txs, Hb: hb}, nil
+}
+
+func (processor *Processor) PushAsSingleTx(ctx context.Context, client table.Client, data dst_table.PushQuery, position types.Position) error {
+	stateParam := table.NewQueryParameters(
+		table.ValueParam("$stateStepId", ydbTypes.Uint64Value(position.Step)),
+		table.ValueParam("$stateTxId", ydbTypes.Uint64Value(position.TxId)),
+	)
+	param := append(data.Parameters, *stateParam...)
+
+	return client.DoTx(ctx,
+		func(ctx context.Context, tx table.TransactionActor) error {
+			_, err := tx.Execute(ctx, data.Query+processor.stateStoreQuery, &param)
+			return err
+		})
+}
+
+func (processor *Processor) DoReplication(ctx context.Context, dstTables []*dst_table.DstTable, dstDb table.Client) error {
+	txDataPerTable := make([][]types.TxData, len(dstTables))
+	batch, err := processor.FormatTx(ctx)
+	if err != nil {
+		return fmt.Errorf("%w; Unable to format tx for destination", err)
+	}
+	for i := 0; i < len(batch.TxData); i++ {
+		txDataPerTable[batch.TxData[i].TableId] = append(txDataPerTable[batch.TxData[i].TableId], batch.TxData[i])
+	}
+	if len(txDataPerTable) != len(dstTables) {
+		return fmt.Errorf("Size of dstTables and tables in the tx mismatched",
+			zap.Int("txDataPertabe", len(txDataPerTable)),
+			zap.Int("dstTable", len(dstTables)))
+	}
+	var query dst_table.PushQuery
+	for i := 0; i < len(txDataPerTable); i++ {
+		q, err := dstTables[i].GenQuery(ctx, txDataPerTable[i], i)
+		if err != nil {
+			return fmt.Errorf("%w; Unable to generate query", err)
+		}
+		query.Query += q.Query
+		query.Parameters = append(query.Parameters, q.Parameters...)
+
+	}
+	xlog.Debug(ctx, "Query to perform", zap.String("query", query.Query))
+	err = processor.PushAsSingleTx(ctx, dstDb, query, types.Position{Step: batch.Hb.Step, TxId: 0})
+	if err != nil {
+		return fmt.Errorf("%w; Unable to push tx", err)
+	}
+	for i := 0; i < len(batch.TxData); i++ {
+		err := batch.TxData[i].CommitTopic()
+		if err != nil {
+			return fmt.Errorf("%w; Unable to commit topic fot dataTx", err)
+		}
+	}
+	xlog.Debug(ctx, "commit hb in topic")
+	err = batch.Hb.CommitTopic()
+	if err != nil {
+		return fmt.Errorf("%w; Unable to commit topic fot Hb", err)
+	}
+	return nil
 }
