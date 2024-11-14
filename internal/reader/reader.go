32c0f063a0a92aa7b850a747728dc64a776e9e4f
Polina Volosnikova <vpolka@nebius.com>
2024-11-14 13:48:36 +0100

    add topic path

--- internal/reader/reader.go
+++ internal/reader/reader.go
@@ -20,7 +20,7 @@ type TopicData struct {
 	Resolved json.RawMessage `json:"resolved"`
 }
 
-func ReadTopic(ctx context.Context, readerId uint32, reader *topicreader.Reader, channel processor.Channel) {
+func ReadTopic(ctx context.Context, topicPath string, readerId uint32, reader *topicreader.Reader, channel processor.Channel) {
 	var mu sync.Mutex
 	lastHb := make(map[int64]uint64)
 	verifyStream := func(part int64, data types.TxData) {
@@ -34,8 +34,8 @@ func ReadTopic(ctx context.Context, readerId uint32, reader *topicreader.Reader,
 		hb := lastHb[part]
 		if hb != 0 && data.Step < hb {
 			errString := fmt.Sprintf("Unexpected step_id in stream, last hb step_id: %v,"+
-				"got tx {\"key\":%v,\"ts\":[%v,%v]}",
-				lastHb[part], serializeKey(data.KeyValues), data.Step, data.TxId)
+				"got tx {\"topic\":\"%v\",\"key\":%v,\"ts\":[%v,%v]}",
+				lastHb[part], topicPath, serializeKey(data.KeyValues), data.Step, data.TxId)
 			stopErr := channel.SaveReplicationState(ctx, processor.REPLICATION_FATAL_ERROR, errString)
 			if stopErr != nil {
 				xlog.Fatal(ctx, errString,
