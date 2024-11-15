b18e757f37040e474bb9b1fe5be0f8ecba0be7b2
VPolka <vpolka@nebius.com>
2024-11-15 16:16:07 +0100

    Merge pull request #47 from ydb-platform/write-all-problem-txs-until-next-hb
    write all problem txs until next hb

--- internal/reader/reader.go
+++ internal/reader/reader.go
@@ -20,18 +20,71 @@ type TopicData struct {
 	Resolved json.RawMessage `json:"resolved"`
 }
 
-func ReadTopic(ctx context.Context, topicPath string, readerId uint32, reader *topicreader.Reader, channel processor.Channel, handler processor.ConflictHandler) {
+func serializeKey(key []json.RawMessage) string {
+	data, err := json.Marshal(key)
+	if err != nil {
+		return "underfined"
+	}
+	return string(data)
+}
+
+func WriteAllProblemTxsUntilNextHb(ctx context.Context, topicPath string, readerId uint32, reader *topicreader.Reader, channel processor.Channel, lastHb map[int64]uint64, hb uint64, partsCount int) {
+	var partsIsDone map[int64]bool
+	for part, partHb := range lastHb {
+		if partHb > hb {
+			partsIsDone[part] = true
+		}
+	}
+	for ctx.Err() == nil && len(partsIsDone) < partsCount {
+		msg, err := reader.ReadMessage(ctx)
+		if err != nil {
+			xlog.Error(ctx, "Unable to read message", zap.Error(err))
+			return
+		}
+		jsonData, err := io.ReadAll(msg)
+		if err != nil {
+			xlog.Error(ctx, "Unable to read all", zap.Error(err))
+			return
+		}
+
+		var topicData TopicData
+		err = json.Unmarshal(jsonData, &topicData)
+		if err != nil {
+			xlog.Error(ctx, "Error parsing topic data", zap.Error(err))
+			return
+		}
+		if topicData.Update != nil || topicData.Erase != nil {
+			data, err := rd.ParseTxData(ctx, jsonData, readerId)
+			if err != nil {
+				xlog.Error(ctx, "ParseTxData: Error parsing tx data", zap.Error(err))
+				return
+			}
+
+			if partHb, ok := lastHb[msg.PartitionID()]; ok && data.Step < partHb {
+				errString := fmt.Sprintf("Unexpected step_id in stream, last hb step_id: %v,"+
+					"got tx {\"topic\":\"%v\",\"key\":%v,\"ts\":[%v,%v]}",
+					hb, topicPath, serializeKey(data.KeyValues), data.Step, data.TxId)
+				xlog.Error(ctx, errString)
+			}
+		} else if topicData.Resolved != nil {
+			data, err := rd.ParseHBData(ctx, jsonData, types.StreamId{readerId, msg.PartitionID()})
+			if err != nil {
+				xlog.Error(ctx, "ParseTxData: Error parsing hb data", zap.Error(err))
+				return
+			}
+			lastHb[msg.PartitionID()] = data.Step
+			if data.Step > hb {
+				partsIsDone[msg.PartitionID()] = true
+			}
+		}
+	}
+}
+
+func ReadTopic(ctx context.Context, topicPath string, readerId uint32, reader *topicreader.Reader, channel processor.Channel, partsCount int, handler processor.ConflictHandler) {
 	var mu sync.Mutex
 	lastHb := make(map[int64]uint64)
 	// returns true - pass item, false - skip item
 	verifyStream := func(part int64, data types.TxData) bool {
-		serializeKey := func(key []json.RawMessage) string {
-			data, err := json.Marshal(key)
-			if err != nil {
-				return "underfined"
-			}
-			return string(data)
-		}
 		hb := lastHb[part]
 
 		if hb != 0 && data.Step < hb {
@@ -63,6 +116,7 @@ func ReadTopic(ctx context.Context, topicPath string, readerId uint32, reader *t
 				"got tx {\"topic\":\"%v\",\"key\":%v,\"ts\":[%v,%v]}",
 				lastHb[part], topicPath, key, data.Step, data.TxId)
 			stopErr := channel.SaveReplicationState(ctx, processor.REPLICATION_FATAL_ERROR, errString)
+			WriteAllProblemTxsUntilNextHb(ctx, topicPath, readerId, reader, channel, lastHb, hb, partsCount)
 			if stopErr != nil {
 				xlog.Fatal(ctx, errString,
 					zap.NamedError("this issue was not stored in the state table due to double error", stopErr))
